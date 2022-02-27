dd7bbd9b7d12ebf3351e22859598e95930233cad
Juyo84 <juliomeme@hotmail.com>
2022-02-27 03:42:49 -0600

    Lista de alumnos

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,70 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+
+  constructor() {}
+
+  alumnos = [
+
+    {
+    
+      "nombre": "Julio",
+      "apellido": "Luevano",
+      "matricula": "1945174",
+      "foto": "/assets/icon/Teams1.jpg"
+    
+    },
+
+    {
+    
+      "nombre": "Manuel",
+      "apellido": "Rivera",
+      "matricula": "4715491",
+      "foto": "/assets/icon/Teams1.jpg"
+
+    },
+
+    {
+    
+      "nombre": "NOMBRE_1",
+      "apellido": "APELLIDO_1",
+      "matricula": "MATRICULA_1",
+      "foto": "/assets/icon/FCFM.jpg"
+
+    },
+
+    {
+    
+      "nombre": "NOMBRE_2",
+      "apellido": "APELLIDO_2",
+      "matricula": "MATRICULA_2",
+      "foto": "/assets/icon/FCFM.jpg"
+    
+    },
+
+    {
+    
+      "nombre": "NOMBRE_3",
+      "apellido": "APELLIDO_3",
+      "matricula": "MATRICULA_3",
+      "foto": "/assets/icon/FCFM.jpg"
+    
+    },
+
+    {
+    
+      "nombre": "Eso Tilin",
+      "apellido": "Vamos Tilin",
+      "matricula": "WOW TILIN",
+      "foto": "/assets/icon/Tilin.jpg"
+    
+    }
+
+  ];
+
+}
